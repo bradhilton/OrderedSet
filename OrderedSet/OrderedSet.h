78b3afbd1cd36b25366f547daff5eef105f77b31
bradhilton <brad.hilton.nw@gmail.com>
2016-02-19 15:30:46 -0700

    Initial Commit

---
+++ OrderedSet/OrderedSet.h
@@ -0,0 +1,19 @@
+//
+//  OrderedSet.h
+//  OrderedSet
+//
+//  Created by Bradley Hilton on 2/19/16.
+//  Copyright © 2016 Brad Hilton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OrderedSet.
+FOUNDATION_EXPORT double OrderedSetVersionNumber;
+
+//! Project version string for OrderedSet.
+FOUNDATION_EXPORT const unsigned char OrderedSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrderedSet/PublicHeader.h>
+
+
