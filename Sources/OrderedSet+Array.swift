0fb2fdb26e6157ae49c51d05ab6971eda95d0eb0
Brad Hilton <brad.hilton.nw@gmail.com>
2016-11-17 12:01:31 -0700

    Update to Swift 3

--- Sources/OrderedSet+Array.swift
+++ Sources/OrderedSet+Array.swift
@@ -6,7 +6,55 @@
 //  Copyright © 2016 Brad Hilton. All rights reserved.
 //
 
-extension OrderedSet : ArrayLiteralConvertible, RangeReplaceableCollectionType {
+extension OrderedSet : ExpressibleByArrayLiteral, RangeReplaceableCollection {
+
+    /// Replaces the specified subrange of elements with the given collection.
+    ///
+    /// This method has the effect of removing the specified range of elements
+    /// from the collection and inserting the new elements at the same location.
+    /// The number of new elements need not match the number of elements being
+    /// removed.
+    ///
+    /// In this example, three elements in the middle of an array of integers are
+    /// replaced by the five elements of a `Repeated<Int>` instance.
+    ///
+    ///      var nums = [10, 20, 30, 40, 50]
+    ///      nums.replaceSubrange(1...3, with: repeatElement(1, count: 5))
+    ///      print(nums)
+    ///      // Prints "[10, 1, 1, 1, 1, 1, 50]"
+    ///
+    /// If you pass a zero-length range as the `subrange` parameter, this method
+    /// inserts the elements of `newElements` at `subrange.startIndex`. Calling
+    /// the `insert(contentsOf:at:)` method instead is preferred.
+    ///
+    /// Likewise, if you pass a zero-length collection as the `newElements`
+    /// parameter, this method removes the elements in the given subrange
+    /// without replacement. Calling the `removeSubrange(_:)` method instead is
+    /// preferred.
+    ///
+    /// Calling this method may invalidate any existing indices for use with this
+    /// collection.
+    ///
+    /// - Parameters:
+    ///   - subrange: The subrange of the collection to replace. The bounds of
+    ///     the range must be valid indices of the collection.
+    ///   - newElements: The new elements to add to the collection.
+    ///
+    /// - Complexity: O(*m*), where *m* is the combined length of the collection
+    ///   and `newElements`. If the call to `replaceSubrange` simply appends the
+    ///   contents of `newElements` to the collection, the complexity is O(*n*),
+    ///   where *n* is the length of `newElements`.
+    public mutating func replaceSubrange<C : Collection>(_ subRange: Range<Index>, with newElements: C) where C.Iterator.Element == IndexingIterator<OrderedSet>.Element {
+        let oldArray = array[subRange]
+        let oldSet = Set(oldArray)
+        let (newArray, newSet) = collapse(newElements)
+        let deletions = oldSet.subtracting(newSet)
+        set.subtract(deletions)
+        set.formUnion(newSet)
+        array.replaceSubrange(subRange, with: newArray)
+        array = array.enumerated().filter { (index, element) in return subRange.contains(index) || subRange.lowerBound == index || !newSet.contains(element) }.map { $0.element }
+    }
+
     
     /// Create an instance containing `elements`.
     public init(arrayLiteral elements: Element...) {
@@ -20,26 +68,13 @@ extension OrderedSet : ArrayLiteralConvertible, RangeReplaceableCollectionType {
     }
     
     /// Construct from an arbitrary sequence with elements of type `Element`.
-    public init<S : SequenceType where S.Generator.Element == Element>(_ s: S) {
+    public init<S : Sequence>(_ s: S) where S.Iterator.Element == Element {
         (self.array, self.set) = collapse(s)
-    }
-    
-    /// Replace the given `subRange` of elements with `newElements`.
-    public mutating func replaceRange<C : CollectionType where C.Generator.Element == IndexingGenerator<OrderedSet>.Element>(subRange: Range<Index>, with newElements: C) {
-        let oldArray = array[subRange]
-        let oldSet = Set(oldArray)
-        let (newArray, newSet) = collapse(newElements)
-        let deletions = oldSet.subtract(newSet)
-        set.subtractInPlace(deletions)
-        set.unionInPlace(newSet)
-        array.replaceRange(subRange, with: newArray)
-        array = array.enumerate().filter { (index, element) in return subRange.contains(index) || subRange.startIndex == index || !newSet.contains(element) }.map { $0.element }
     }
 
     public var capacity: Int {
         return array.capacity
     }
-    
     
     /// If `!self.isEmpty`, remove the last element and return it, otherwise
     /// return `nil`.
@@ -52,6 +87,6 @@ extension OrderedSet : ArrayLiteralConvertible, RangeReplaceableCollectionType {
 }
 
 /// Operator form of `appendContentsOf`.
-public func +=<Element, S : SequenceType where S.Generator.Element == Element>(inout lhs: OrderedSet<Element>, rhs: S) {
-    lhs.appendContentsOf(rhs)
+public func +=<Element, S : Sequence>(lhs: inout OrderedSet<Element>, rhs: S) where S.Iterator.Element == Element {
+    lhs.append(contentsOf: rhs)
 }
--- Sources/OrderedSet.swift
+++ Sources/OrderedSet.swift
@@ -7,20 +7,44 @@
 //
 
 /// An ordered collection of unique `Element` instances
-public struct OrderedSet<Element : Hashable> : Hashable, CollectionType, MutableCollectionType {
+public struct OrderedSet<Element : Hashable> : Hashable, Collection, MutableCollection {
     
     internal(set) var array: [Element]
     internal(set) var set: Set<Element>
     
-    /// Always zero, which is the index of the first element when non-empty.
+    /// The position of the first element in a nonempty collection.
+    ///
+    /// If the collection is empty, `startIndex` is equal to `endIndex`.
     public var startIndex: Int {
         return array.startIndex
     }
     
-    /// A "past-the-end" element index; the successor of the last valid
-    /// subscript argument.
+    /// The collection's "past the end" position---that is, the position one
+    /// greater than the last valid subscript argument.
+    ///
+    /// When you need a range that includes the last element of a collection, use
+    /// the half-open range operator (`..<`) with `endIndex`. The `..<` operator
+    /// creates a range that doesn't include the upper bound, so it's always
+    /// safe to use with `endIndex`. For example:
+    ///
+    ///     let numbers = [10, 20, 30, 40, 50]
+    ///     if let index = numbers.index(of: 30) {
+    ///         print(numbers[index ..< numbers.endIndex])
+    ///     }
+    ///     // Prints "[30, 40, 50]"
+    ///
+    /// If the collection is empty, `endIndex` is equal to `startIndex`.
     public var endIndex: Int {
         return array.endIndex
+    }
+
+    /// Returns the position immediately after the given index.
+    ///
+    /// - Parameter i: A valid index of the collection. `i` must be less than
+    ///   `endIndex`.
+    /// - Returns: The index value immediately after `i`.
+    public func index(after i: Int) -> Int {
+        return i + 1
     }
     
     public subscript(position: Int) -> Element {
@@ -32,7 +56,7 @@ public struct OrderedSet<Element : Hashable> : Hashable, CollectionType, Mutable
             set.remove(oldValue)
             array[position] = newValue
             set.insert(newValue)
-            array = array.enumerate().filter { (index, element) in return index == position || element.hashValue != newValue.hashValue }.map { $0.element }
+            array = array.enumerated().filter { (index, element) in return index == position || element.hashValue != newValue.hashValue }.map { $0.element }
         }
     }
     
@@ -42,7 +66,7 @@ public struct OrderedSet<Element : Hashable> : Hashable, CollectionType, Mutable
     
 }
 
-@warn_unused_result
+
 public func ==<T : Hashable>(lhs: OrderedSet<T>, rhs: OrderedSet<T>) -> Bool {
     return lhs.set == rhs.set
 }
