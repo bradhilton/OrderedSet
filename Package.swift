c6490dddf0ec3751b24b9ff47d65724597490acb
Jeremy Saklad <saklad5@me.com>
2020-03-15 13:02:12 -0500

    Add test target to Package.swift
    This is necessary for package testing to work. Similar to the main target, it needs to be in a special subfolder of Tests.

--- Package.swift
+++ Package.swift
@@ -3,13 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "OrderedSet",
-	products: [
-		.library(name: "OrderedSet", targets: ["OrderedSet"])
-	],
+    products: [
+        .library(name: "OrderedSet", targets: ["OrderedSet"])
+    ],
     targets: [
-        .target(
-            name: "OrderedSet",
-            dependencies: []
-        )
+        .target(name: "OrderedSet", dependencies: []),
+        .testTarget(name: "OrderedSetTests", dependencies: ["OrderedSet"])
     ]
 )
